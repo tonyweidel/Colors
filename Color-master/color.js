ca9a18ce1377f7265fc257ed5d39cffbcac53b82
Tony Weidel <8aweidel@stpatrick.org>
2016-11-02 14:04:28 -0500

    I made color stuff in javascript yay me

---
+++ Color-master/color.js
@@ -0,0 +1,44 @@
+//Declare the Color Object with our new keyword below here.
+
+const Color = new Object();
+
+/*
+** rgb is an array that hold the color values.
+** first is red -> R, then green -> G, and blue -> B
+** Example
+*/
+
+Color.isColor = function(rgb){
+  //each color must be between 0 and 255. Also can be equal.
+
+  let redworks = (rgb[0] >= 0) && (rgb[0] <= 255);
+  let greenworks = (rgb[1] >= 0) && (rgb[1] <= 255);
+  let blueworks = (rgb[2] >= 0) && (rgb[2] <= 255);
+
+  if(redworks && greenworks && blueworks){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+Color.redIntensity = function(rgb){
+  Math.round((redworks/255)*100);
+
+};
+
+//greenIntensity
+
+Color.greenIntensity = function(rgb){
+  Math.round((rgb[0]/255)*100);
+
+//blueIntensity
+
+Color.blueIntensity = function(rgb){
+  Math.round((rgb[0]/255)*100);
+
+//brightness
+
+
+//complement
